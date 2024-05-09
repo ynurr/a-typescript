7c72a8dfe59085cda4eb425d707ef2c82a2f01ee
ynurr <0305_08@naver.com>
2024-05-09 23:36:24 +0900

    object 타입

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-var 이름 = 'kim';
+var 내최애 = { song: '노래제목', singer: '가수이름' };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-let 이름 :string = 'kim';
+let 내최애 :{ song : string, singer : string } = { song : '노래제목', singer : '가수이름' };
+
+let project :{
+    member : string[],
+    days : number,
+    started : boolean,
+} = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
+}
