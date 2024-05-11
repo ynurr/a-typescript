7166030c638b3a17b9a6488270a23bc8cf884814
ynurr <0305_08@naver.com>
2024-05-11 22:13:30 +0900

    Narrowing

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-var 내최애 = { song: '노래제목', singer: '가수이름' };
-var project = {
-    member: ['kim', 'park'],
-    days: 30,
-    started: true,
-};
+function 클리닝(x) {
+    var result = [];
+    x.forEach(function (a) {
+        console.log('a:' + a);
+        if (typeof a === 'string') {
+            result.push(parseFloat(a));
+        }
+        else {
+            result.push(a);
+        }
+    });
+    return result;
+}
+console.log(클리닝([123, '5', '231', 221]));
+var 철수쌤 = { subject: 'math' };
+var 영희쌤 = { subject: ['science', 'english'] };
+var 민수쌤 = { subject: ['science', 'art', 'korean'] };
+function 담당과목(x) {
+    if (typeof x.subject === 'string') {
+        return x.subject;
+    }
+    else if (Array.isArray(x.subject)) {
+        return x.subject[x.subject.length - 1];
+    }
+    else {
+        return '없음';
+    }
+}
+console.log(담당과목({ subject: ['english', 'art'] }));
--- index.ts
+++ index.ts
@@ -1,11 +1,35 @@
-let 내최애 :{ song : string, singer : string } = { song : '노래제목', singer : '가수이름' };
+function 클리닝(x: (string | number)[]) {
+    let result: number[] = [];
+    
+    x.forEach(a => {
+        console.log('a:'+a);
+        if (typeof a === 'string') {
+            result.push(parseFloat(a));
+        } else {
+            result.push(a);
+        }
+    })
 
-let project :{
-    member : string[],
-    days : number,
-    started : boolean,
-} = {
-    member : ['kim', 'park'],
-    days : 30,
-    started : true,
+    return result;
 }
+
+console.log(클리닝([123,'5','231',221]));
+
+
+
+
+let 철수쌤 = { subject : 'math' }
+let 영희쌤 = { subject : ['science', 'english'] }
+let 민수쌤 = { subject : ['science', 'art', 'korean'] }
+
+function 담당과목(x: {subject: string | string[]}) {
+    if (typeof x.subject === 'string') {
+        return x.subject;
+    } else if (Array.isArray(x.subject)) {
+        return x.subject[x.subject.length-1];
+    } else {
+        return '없음';
+    } 
+}
+
+console.log(담당과목({subject : ['english', 'art']}))
